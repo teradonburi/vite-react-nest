c3ca4eb34f82c8bc0fc17f218da5866547756485
teradonburi <daikiterai@gmail.com>
2024-09-16 17:51:20 +0900

    fix

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,21 +1,21 @@
-import { resolve } from 'path';
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import { resolve } from "node:path";
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  root: 'src',
-  plugins: [react()],
-  build: {
-    outDir: resolve(__dirname, 'dist'),
-    emptyOutDir: true,
-    rollupOptions: {
-      input: {
-        '': resolve(__dirname, 'src/index.html'),
-      },
-      output: {
-        entryFileNames: `assets/[name]/bundle.js`,
-      },
-    },
-  },
+	root: "src",
+	plugins: [react()],
+	build: {
+		outDir: resolve(__dirname, "dist"),
+		emptyOutDir: true,
+		rollupOptions: {
+			input: {
+				"": resolve(__dirname, "src/index.html"),
+			},
+			output: {
+				entryFileNames: "assets/[name]/bundle.js",
+			},
+		},
+	},
 });

6e1ad64e33d7137c857d9c3119d9304fbc2d4352
teradonburi <daikiterai@gmail.com>
2024-09-16 23:53:58 +0900

    swagger

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,3 +1,4 @@
+import * as fs from "node:fs";
 import { NestFactory } from "@nestjs/core";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import { AppModule } from "./app.module";
@@ -14,6 +15,9 @@ async function bootstrap() {
 		.build();
 	const document = SwaggerModule.createDocument(app, config);
 	SwaggerModule.setup("api-docs", app, document);
+
+	// JSON
+	fs.writeFileSync("./swagger.json", JSON.stringify(document, undefined, 2));
 
 	await app.listen(process.env.PORT || 3000);
 }

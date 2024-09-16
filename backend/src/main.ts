55976bdd8c62166be9978c96db86e0c9b9e7edf6
teradonburi <daikiterai@gmail.com>
2024-09-16 23:52:15 +0900

    swagger

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from "@nestjs/core";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 	app.setGlobalPrefix("api");
+
+	const config = new DocumentBuilder()
+		.setTitle("api example")
+		.setDescription("The API description")
+		.setVersion("1.0")
+		.addTag("example")
+		.build();
+	const document = SwaggerModule.createDocument(app, config);
+	SwaggerModule.setup("api-docs", app, document);
+
 	await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
